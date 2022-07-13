c0208ff55caee34db6913ce35a2e9c9ed39d46fb
Leesin0222 <dragon02229@naver.com>
2022-07-13 23:22:14 +0900

    ⚡ :: 바텀네비게이션 구현

--- app/src/main/java/com/yongjincompany/dsmtcg/MainActivity.kt
+++ app/src/main/java/com/yongjincompany/dsmtcg/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.yongjincompany.dsmtcg
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.semicolon.walkhub.ui.base.BaseActivity
+import com.yongjincompany.dsmtcg.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    private fun initBottomNav() {
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.navHostFragmentActivityMain)?.findNavController()
+        val nav = binding.bottomNav as BottomNavigationView
+        navController?.let {
+            nav.setupWithNavController(navController)
+        }
+    }
+
+    override fun initView() {
+        initBottomNav()
     }
 }
