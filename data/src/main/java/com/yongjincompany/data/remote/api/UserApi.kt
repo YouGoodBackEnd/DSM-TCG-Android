0798910dff097a99be451859c812d5fb7c31bbe4
Leesin0222 <dragon02229@naver.com>
2022-07-21 14:11:10 +0900

    ⚡ :: 회원가입, 로그인 api 작성

---
+++ data/src/main/java/com/yongjincompany/data/remote/api/UserApi.kt
@@ -0,0 +1,20 @@
+package com.yongjincompany.data.remote.api
+
+import com.yongjincompany.data.remote.request.users.UserRegisterRequest
+import com.yongjincompany.data.remote.request.users.UserSignInRequest
+import com.yongjincompany.data.remote.response.users.UserRegisterResponse
+import com.yongjincompany.data.remote.response.users.UserSignInResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UserApi {
+    @POST("users")
+    suspend fun userRegister(
+        @Body userSignUpRequest: UserRegisterRequest
+    ): UserRegisterResponse
+
+    @POST("users/auth")
+    suspend fun userSignIn(
+        @Body userSignInRequest: UserSignInRequest
+    ): UserSignInResponse
+}
