5e20ce76bba29422240976019ed344d73913b6ec
Leesin0222 <dragon02229@naver.com>
2022-07-23 00:14:20 +0900

    ⚡ :: 로그인 ,회원가입 화면으로 이동 구현

--- presentation/src/main/java/com/yongjincompany/dsmtcg/ui/StartActivity.kt
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/ui/StartActivity.kt
@@ -1,8 +1,10 @@
 package com.yongjincompany.dsmtcg.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.gun0912.tedpermission.provider.TedPermissionProvider.context
 import com.yongjincompany.dsmtcg.R
 import com.yongjincompany.dsmtcg.databinding.ActivityStartBinding
 
@@ -13,5 +15,15 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_start)
+
+        binding.btJoin.setOnClickListener {
+            val intent = Intent(context, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(context, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
