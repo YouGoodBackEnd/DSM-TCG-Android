19b4be58fdaad294f10da055ea98b6801eb8abcc
Leesin0222 <dragon02229@naver.com>
2022-07-24 13:17:11 +0900

    ⚡ :: 내 정보 수정 API 작성

--- data/src/main/java/com/yongjincompany/data/remote/api/UserApi.kt
+++ data/src/main/java/com/yongjincompany/data/remote/api/UserApi.kt
@@ -2,9 +2,11 @@ package com.yongjincompany.data.remote.api
 
 import com.yongjincompany.data.remote.request.users.UserRegisterRequest
 import com.yongjincompany.data.remote.request.users.UserSignInRequest
+import com.yongjincompany.data.remote.request.users.updateMyInfoRequest
 import com.yongjincompany.data.remote.response.users.UserRegisterResponse
 import com.yongjincompany.data.remote.response.users.UserSignInResponse
 import retrofit2.http.Body
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface UserApi {
@@ -17,4 +19,9 @@ interface UserApi {
     suspend fun userSignIn(
         @Body userSignInRequest: UserSignInRequest
     ): UserSignInResponse
+
+    @PATCH
+    suspend fun updateMyInfo(
+        @Body updateMyInfoRequest: updateMyInfoRequest
+    )
 }
