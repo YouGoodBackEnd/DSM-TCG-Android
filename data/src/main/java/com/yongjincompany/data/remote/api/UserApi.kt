6436cc7d2daa564840b7aef39008e7914ff56403
Leesin0222 <dragon02229@naver.com>
2022-07-25 23:38:14 +0900

    ⚡ :: 내 정보 조회 api 작성 및 내 정보 수정 Request 변경

--- data/src/main/java/com/yongjincompany/data/remote/api/UserApi.kt
+++ data/src/main/java/com/yongjincompany/data/remote/api/UserApi.kt
@@ -1,27 +1,34 @@
 package com.yongjincompany.data.remote.api
 
+import com.yongjincompany.data.remote.request.users.UpdateMyInfoRequest
 import com.yongjincompany.data.remote.request.users.UserRegisterRequest
 import com.yongjincompany.data.remote.request.users.UserSignInRequest
-import com.yongjincompany.data.remote.request.users.updateMyInfoRequest
+import com.yongjincompany.data.remote.response.users.FetchMyInfoResponse
 import com.yongjincompany.data.remote.response.users.UserRegisterResponse
 import com.yongjincompany.data.remote.response.users.UserSignInResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface UserApi {
     @POST("users")
     suspend fun userRegister(
-        @Body userSignUpRequest: UserRegisterRequest
+        @Body userSignUpRequest: UserRegisterRequest,
     ): UserRegisterResponse
 
     @POST("users/auth")
     suspend fun userSignIn(
-        @Body userSignInRequest: UserSignInRequest
+        @Body userSignInRequest: UserSignInRequest,
     ): UserSignInResponse
 
     @PATCH
     suspend fun updateMyInfo(
-        @Body updateMyInfoRequest: updateMyInfoRequest
+        @Body updateMyInfoRequest: UpdateMyInfoRequest,
     )
+
+    @GET
+    suspend fun fetchMyInfo(
+    ): FetchMyInfoResponse
+
 }
