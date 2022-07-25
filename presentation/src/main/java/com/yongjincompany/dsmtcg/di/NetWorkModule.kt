3a4b7b44eb632f6826d25f33617b7353f222c250
Leesin0222 <dragon02229@naver.com>
2022-07-26 00:04:23 +0900

    ⚡ :: NetWorkModule 작성

---
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/di/NetWorkModule.kt
@@ -0,0 +1,75 @@
+package com.yongjincompany.dsmtcg.di
+
+import com.yongjincompany.data.remote.api.*
+import com.yongjincompany.data.interceptor.AuthorizationInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.engineio.client.transports.WebSocket
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetWorkModule {
+    private const val BASE_URL = "https://"
+    private const val SOCKET_BASE_URL = "http://"
+
+    @Provides
+    fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        authorizationInterceptor: AuthorizationInterceptor,
+    ): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(authorizationInterceptor)
+            .build()
+
+    @Provides
+    fun provideOptions(
+        okHttpClient: OkHttpClient,
+    ): IO.Options {
+        val options = IO.Options()
+        options.callFactory = okHttpClient
+        options.webSocketFactory = okHttpClient
+        options.transports = arrayOf(WebSocket.NAME)
+        return options
+    }
+
+    @Provides
+    fun provideSocket(
+        options: IO.Options,
+    ): Socket {
+        return IO.socket(SOCKET_BASE_URL, options)
+    }
+
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    fun provideUserApi(retrofit: Retrofit): UserApi =
+        retrofit.create(UserApi::class.java)
+
+    @Provides
+    fun provideImageApi(retrofit: Retrofit): ImagesApi =
+        retrofit.create(ImagesApi::class.java)
+}
