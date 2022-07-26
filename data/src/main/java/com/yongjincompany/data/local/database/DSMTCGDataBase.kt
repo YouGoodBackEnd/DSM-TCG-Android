e6efd1736f1d5ca0072e0a7d3cd51a8d3c1b43c6
Leesin0222 <dragon02229@naver.com>
2022-07-27 07:20:28 +0900

    ⚡ :: DataBase 작성

---
+++ data/src/main/java/com/yongjincompany/data/local/database/DSMTCGDataBase.kt
@@ -0,0 +1,24 @@
+package com.yongjincompany.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.yongjincompany.data.local.dao.UserDao
+import com.yongjincompany.data.local.entity.user.FetchMyInfoRoomEntity
+
+@Database(
+    entities = [
+        FetchMyInfoRoomEntity::class
+    ], version = 1, exportSchema = false
+)
+
+@TypeConverters(
+    value = [
+    ]
+
+)
+
+abstract class DSMTCGDataBase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+}
