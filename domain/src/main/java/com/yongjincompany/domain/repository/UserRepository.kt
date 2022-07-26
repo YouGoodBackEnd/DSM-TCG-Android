7793092ecd644362defd045e8c66621e136dfbd0
Leesin0222 <dragon02229@naver.com>
2022-07-27 07:21:19 +0900

    ⚡ :: Repository에 내 정보 조회 interface 코드 작성

--- domain/src/main/java/com/yongjincompany/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/yongjincompany/domain/repository/UserRepository.kt
@@ -1,21 +1,25 @@
 package com.yongjincompany.domain.repository
 
+import com.yongjincompany.domain.entity.users.FetchMyInfoEntity
 import com.yongjincompany.domain.param.user.PostUserRegisterParam
 import com.yongjincompany.domain.param.user.PostUserSignInParam
 import com.yongjincompany.domain.param.user.UpdateMyInfoParam
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     suspend fun postUserRegister(
-        postUserRegisterParam: PostUserRegisterParam
+        postUserRegisterParam: PostUserRegisterParam,
     )
 
     suspend fun postUserSignIn(
-        postUserSignInParam: PostUserSignInParam
+        postUserSignInParam: PostUserSignInParam,
     )
 
     suspend fun autoLogin()
 
     suspend fun updateMyInfo(
-        updateMyInfoParam: UpdateMyInfoParam
+        updateMyInfoParam: UpdateMyInfoParam,
     )
+
+    suspend fun fetchMyInfo(): Flow<FetchMyInfoEntity>
 }
