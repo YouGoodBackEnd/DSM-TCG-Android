31511b6d3f0f8458a68e1d0ff83e1cee4c979274
Leesin0222 <dragon02229@naver.com>
2022-07-30 00:34:30 +0900

    ⚡ :: RemoteUserDataSourceImpl 계정 삭제 작성

--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteUserDataSourceImpl.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteUserDataSourceImpl.kt
@@ -47,4 +47,10 @@ class RemoteUserDataSourceImpl @Inject constructor(
             .httpRequest { userApi.logOut() }
             .sendRequest()
     }
+
+    override suspend fun deleteAccount() {
+        HttpHandler<Unit>()
+            .httpRequest { userApi.deleteAccount() }
+            .sendRequest()
+    }
 }
