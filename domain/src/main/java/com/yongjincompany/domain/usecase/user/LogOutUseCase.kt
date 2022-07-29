e25137c4d2300720a36da5ebdad26c452d39c4d2
Leesin0222 <dragon02229@naver.com>
2022-07-29 23:17:18 +0900

    ⚡ :: 로그아웃 UseCase 작성

---
+++ domain/src/main/java/com/yongjincompany/domain/usecase/user/LogOutUseCase.kt
@@ -0,0 +1,13 @@
+package com.yongjincompany.domain.usecase.user
+
+import com.yongjincompany.domain.repository.UserRepository
+import com.yongjincompany.domain.usecase.UseCase
+import javax.inject.Inject
+
+class LogOutUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) : UseCase<Unit, Unit>() {
+
+    override suspend fun execute(data: Unit): Unit =
+        userRepository.logOut()
+}
