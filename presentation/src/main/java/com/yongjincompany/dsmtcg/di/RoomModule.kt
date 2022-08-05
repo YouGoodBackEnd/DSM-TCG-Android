6c6ee15fb72868c8d8d727f0edfd9922752f6207
Leesin0222 <dragon02229@naver.com>
2022-08-05 20:57:39 +0900

    ⚡ :: RoomModule 작성

---
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/di/RoomModule.kt
@@ -0,0 +1,42 @@
+package com.yongjincompany.dsmtcg.di
+
+import android.content.Context
+import androidx.room.Room
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.yongjincompany.data.local.dao.UserDao
+import com.yongjincompany.data.local.database.DSMTCGDataBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    @Singleton
+    @Provides
+    fun providesMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    fun provideWalkHubDatabase(
+        @ApplicationContext context: Context,
+        moshi: Moshi,
+    ): DSMTCGDataBase = Room
+        .databaseBuilder(context, DSMTCGDataBase::class.java, "DSMDataBase")
+
+        .build()
+
+    @Provides
+    fun provideUserDao(
+        dsmtcgDataBase: DSMTCGDataBase,
+    ): UserDao = dsmtcgDataBase.userDao()
+
+}
