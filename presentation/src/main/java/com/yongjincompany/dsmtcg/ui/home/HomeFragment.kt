65a24d3bc18b2943eb870d842b5a44be7e596aa8
Leesin0222 <dragon02229@naver.com>
2022-08-05 20:34:19 +0900

    ⚡ :: 마이페이지로 넘어가는 코드 작성

--- presentation/src/main/java/com/yongjincompany/dsmtcg/ui/home/HomeFragment.kt
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/ui/home/HomeFragment.kt
@@ -1,23 +1,32 @@
 package com.yongjincompany.dsmtcg.ui.home
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yongjincompany.dsmtcg.R
+import com.yongjincompany.dsmtcg.base.BaseFragment
+import com.yongjincompany.dsmtcg.databinding.FragmentHomeBinding
+import com.yongjincompany.dsmtcg.ui.MyPageActivity
 
 
-class HomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
+class  HomeFragment: BaseFragment<FragmentHomeBinding>(
+    R.layout.fragment_home
+) {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
+    override fun initView() {
+        binding.userCardview.setOnClickListener {
+            val intent = Intent(context, MyPageActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
