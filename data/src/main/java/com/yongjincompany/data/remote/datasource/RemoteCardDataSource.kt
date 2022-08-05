a52c08c7f52891fcb4ae200956369e0c5a8b9194
Leesin0222 <dragon02229@naver.com>
2022-08-05 16:08:56 +0900

    ⚡ :: remoteCardDataSource에 내 카드 조회 작성

--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteCardDataSource.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteCardDataSource.kt
@@ -1,7 +1,10 @@
 package com.yongjincompany.data.remote.datasource
 
+import com.yongjincompany.data.remote.response.cards.FetchMyCardItemResponse
 import com.yongjincompany.data.remote.response.cards.FreeCardResponse
 
 interface RemoteCardDataSource {
     suspend fun drawFreeCard(): FreeCardResponse
+
+    suspend fun fetchMyCard(): List<FetchMyCardItemResponse>
 }
