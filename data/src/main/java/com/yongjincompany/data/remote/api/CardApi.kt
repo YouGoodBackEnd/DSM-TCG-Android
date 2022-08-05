1b325947098f7ca13d296c545a288a0caa512f73
Leesin0222 <dragon02229@naver.com>
2022-08-05 16:06:38 +0900

    ⚡ :: 내 카드 받아오기(조회) api 작성

--- data/src/main/java/com/yongjincompany/data/remote/api/CardApi.kt
+++ data/src/main/java/com/yongjincompany/data/remote/api/CardApi.kt
@@ -1,10 +1,16 @@
 package com.yongjincompany.data.remote.api
 
+import com.yongjincompany.data.remote.response.cards.FetchMyCardItemResponse
 import com.yongjincompany.data.remote.response.cards.FreeCardResponse
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface CardApi {
     @POST("cards/draw/free")
     suspend fun drawFreeCard(
-    ) : FreeCardResponse
+    ): FreeCardResponse
+
+    @GET("cards")
+    suspend fun fetchMyCard(
+    ): List<FetchMyCardItemResponse>
 }
