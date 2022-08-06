ceba6bd88a1593323dab28f36471ca05a1b01ecf
Leesin0222 <dragon02229@naver.com>
2022-08-07 02:19:34 +0900

    ⚡ :: DataBase에 CardDao 작성

--- data/src/main/java/com/yongjincompany/data/local/database/DSMTCGDataBase.kt
+++ data/src/main/java/com/yongjincompany/data/local/database/DSMTCGDataBase.kt
@@ -3,6 +3,7 @@ package com.yongjincompany.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.yongjincompany.data.local.dao.CardDao
 import com.yongjincompany.data.local.dao.UserDao
 import com.yongjincompany.data.local.entity.user.FetchMyInfoRoomEntity
 
@@ -20,5 +21,5 @@ import com.yongjincompany.data.local.entity.user.FetchMyInfoRoomEntity
 
 abstract class DSMTCGDataBase : RoomDatabase() {
     abstract fun userDao(): UserDao
-
+    abstract fun cardDao(): CardDao
 }
