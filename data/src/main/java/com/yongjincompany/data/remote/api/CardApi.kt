93bac8674cf1df65f81be01220214abb3336be84
Leesin0222 <dragon02229@naver.com>
2022-08-06 21:38:12 +0900

    ⚡ :: fetchMyCard 반환 값 변경

--- data/src/main/java/com/yongjincompany/data/remote/api/CardApi.kt
+++ data/src/main/java/com/yongjincompany/data/remote/api/CardApi.kt
@@ -1,6 +1,6 @@
 package com.yongjincompany.data.remote.api
 
-import com.yongjincompany.data.remote.response.cards.FetchMyCardItemResponse
+import com.yongjincompany.data.remote.response.cards.FetchMyCardResponse
 import com.yongjincompany.data.remote.response.cards.FreeCardResponse
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -12,5 +12,5 @@ interface CardApi {
 
     @GET("cards")
     suspend fun fetchMyCard(
-    ): List<FetchMyCardItemResponse>
+    ): FetchMyCardResponse
 }
