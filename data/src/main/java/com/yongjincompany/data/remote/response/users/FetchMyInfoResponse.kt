59ea94932ac2381138e2b734a8aa2bc1dee49767
Leesin0222 <dragon02229@naver.com>
2022-08-08 03:17:01 +0900

    ⚡ :: 바뀐 명세에 따라 FetchMyInfoResponse 수정
    account_id 제거 및 card_count 추가됨

--- data/src/main/java/com/yongjincompany/data/remote/response/users/FetchMyInfoResponse.kt
+++ data/src/main/java/com/yongjincompany/data/remote/response/users/FetchMyInfoResponse.kt
@@ -8,8 +8,25 @@ data class FetchMyInfoResponse(
     @SerializedName("profile_url") val profileUrl: String,
     @SerializedName("rank") val rank: Int,
     @SerializedName("user_id") val userId: Long,
-    @SerializedName("account_id") val accountId: String
-)
+    @SerializedName("card_count") val cardCount: CardCount,
+) {
+    data class CardCount(
+        @SerializedName("agrade_card_count") val agradeCardCount: Int,
+        @SerializedName("bgrade_card_count") val bgradeCardCount: Int,
+        @SerializedName("cgrade_card_count") val cgradeCardCount: Int,
+        @SerializedName("sgrade_card_count") val sgradeCardCount: Int,
+        @SerializedName("ssgrade_card_count") val ssgradeCardCount: Int,
+    )
+
+    fun CardCount.toEntity() =
+        FetchMyInfoEntity.CardCount(
+            agradeCardCount = agradeCardCount,
+            bgradeCardCount = bgradeCardCount,
+            cgradeCardCount = cgradeCardCount,
+            sgradeCardCount = sgradeCardCount,
+            ssgradeCardCount = ssgradeCardCount
+        )
+}
 
 fun FetchMyInfoResponse.toEntity() =
     FetchMyInfoEntity(
@@ -17,5 +34,5 @@ fun FetchMyInfoResponse.toEntity() =
         profileUrl = profileUrl,
         rank = rank,
         userId = userId,
-        accountId = accountId
+        cardCount = cardCount.toEntity()
     )
