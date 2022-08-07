aeef5bc0d588bf985d5157611f1b46b8f8c4cf9e
Leesin0222 <dragon02229@naver.com>
2022-08-07 22:24:16 +0900

    ⚡ :: CardDao 내 카드 조회 쿼리문 insert문 작성

--- data/src/main/java/com/yongjincompany/data/local/dao/CardDao.kt
+++ data/src/main/java/com/yongjincompany/data/local/dao/CardDao.kt
@@ -1,7 +1,16 @@
 package com.yongjincompany.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.yongjincompany.data.local.entity.card.FetchMyCardRoomEntity
 
 @Dao
 interface CardDao {
+    @Query("SELECT * FROM myCard")
+    suspend fun fetchMyCard(): FetchMyCardRoomEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMyCard(fetchMyCardRoomEntity: FetchMyCardRoomEntity)
 }
