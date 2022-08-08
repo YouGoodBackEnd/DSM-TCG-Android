6bf71813f8b07290c3fad1d90a2a9cc232f7577e
Leesin0222 <dragon02229@naver.com>
2022-08-08 14:16:54 +0900

    ⚡ ::  CardRepository에  fetchMyCard 작성

--- domain/src/main/java/com/yongjincompany/domain/repository/CardRepository.kt
+++ domain/src/main/java/com/yongjincompany/domain/repository/CardRepository.kt
@@ -1,9 +1,12 @@
 package com.yongjincompany.domain.repository
 
+import com.yongjincompany.domain.entity.cards.FetchMyCardEntity
 import com.yongjincompany.domain.entity.cards.FreeCardEntity
 import kotlinx.coroutines.flow.Flow
 
 interface CardRepository {
 
     suspend fun drawFreeCard(): Flow<FreeCardEntity>
+
+    suspend fun fetchMyCard(): Flow<FetchMyCardEntity>
 }
