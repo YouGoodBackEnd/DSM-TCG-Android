4fb40368e7a5b7f411da924273baef196051223f
Leesin0222 <dragon02229@naver.com>
2022-08-14 18:50:30 +0900

    ⚰️ :: Card 카테고리에서 drawFreeCard 관련 로직 전부 제거

--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteCardDataSource.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteCardDataSource.kt
@@ -1,11 +1,7 @@
 package com.yongjincompany.data.remote.datasource
 
-import com.yongjincompany.data.remote.response.cards.FetchMyCardResponse
-import com.yongjincompany.data.remote.response.cards.FreeCardResponse
 import com.yongjincompany.domain.entity.cards.FetchMyCardEntity
 
 interface RemoteCardDataSource {
-    suspend fun drawFreeCard(): FreeCardResponse
-
     suspend fun fetchMyCard(): FetchMyCardEntity
 }
--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteCardDataSourceImpl.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteCardDataSourceImpl.kt
@@ -2,7 +2,6 @@ package com.yongjincompany.data.remote.datasource
 
 import com.yongjincompany.data.remote.api.CardApi
 import com.yongjincompany.data.remote.response.cards.FetchMyCardResponse
-import com.yongjincompany.data.remote.response.cards.FreeCardResponse
 import com.yongjincompany.data.remote.response.cards.toEntity
 import com.yongjincompany.data.util.HttpHandler
 import com.yongjincompany.domain.entity.cards.FetchMyCardEntity
@@ -11,11 +10,6 @@ import javax.inject.Inject
 class RemoteCardDataSourceImpl @Inject constructor(
     private val cardApi: CardApi,
 ) : RemoteCardDataSource {
-    override suspend fun drawFreeCard(): FreeCardResponse =
-        HttpHandler<FreeCardResponse>()
-            .httpRequest { cardApi.drawFreeCard() }
-            .sendRequest()
-
     override suspend fun fetchMyCard(): FetchMyCardEntity =
         HttpHandler<FetchMyCardResponse>()
             .httpRequest { cardApi.fetchMyCard() }
