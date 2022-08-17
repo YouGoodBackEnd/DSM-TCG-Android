3ace52690796c690113d1912c3a461cd98629996
Leesin0222 <dragon02229@naver.com>
2022-08-17 18:31:14 +0900

    ⚡ :: 무료 상자 시간 조회 repository 구현체 작성

---
+++ data/src/main/java/com/yongjincompany/data/repository/ChestRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.yongjincompany.data.repository
+
+import com.yongjincompany.data.remote.datasource.RemoteChestDataSource
+import com.yongjincompany.data.util.OfflineCacheUtil
+import com.yongjincompany.domain.entity.chests.FetchFreeChestTimeEntity
+import com.yongjincompany.domain.repository.ChestRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ChestRepositoryImpl @Inject constructor(
+    private val remoteChestDataSource: RemoteChestDataSource
+): ChestRepository {
+    override suspend fun fetchFreeChestTime(): Flow<FetchFreeChestTimeEntity> =
+        OfflineCacheUtil<FetchFreeChestTimeEntity>()
+            .remoteData { remoteChestDataSource.fetchFreeChsetTime() }
+            .createFlow()
+}
