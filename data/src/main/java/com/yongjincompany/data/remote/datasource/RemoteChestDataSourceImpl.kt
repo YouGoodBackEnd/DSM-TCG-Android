2e14cc244a88fa44f58cede6ed0ab8ae6c7eecef
Leesin0222 <dragon02229@naver.com>
2022-08-17 21:27:51 +0900

    ⚡ :: 스폐셜 상자 오픈 시간 조회 RemoteChestDataSource구현체 작성

--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChestDataSourceImpl.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChestDataSourceImpl.kt
@@ -2,9 +2,11 @@ package com.yongjincompany.data.remote.datasource
 
 import com.yongjincompany.data.remote.api.ChestApi
 import com.yongjincompany.data.remote.response.chests.FreeChestOpenResponse
+import com.yongjincompany.data.remote.response.chests.SpecialChestOpenResponse
 import com.yongjincompany.data.remote.response.chests.toEntity
 import com.yongjincompany.data.util.HttpHandler
 import com.yongjincompany.domain.entity.chests.FetchFreeChestTimeEntity
+import com.yongjincompany.domain.entity.chests.FetchSpecialChestTimeEntity
 import javax.inject.Inject
 
 class RemoteChestDataSourceImpl @Inject constructor(
@@ -13,5 +15,10 @@ class RemoteChestDataSourceImpl @Inject constructor(
     override suspend fun fetchFreeChestTime(): FetchFreeChestTimeEntity =
         HttpHandler<FreeChestOpenResponse>()
             .httpRequest { chestApi.getFreeChestTime() }
+            .sendRequest().toEntity()
+
+    override suspend fun fetchSpecialChestTime(): FetchSpecialChestTimeEntity =
+        HttpHandler<SpecialChestOpenResponse>()
+            .httpRequest { chestApi.getSpecialChestTime() }
             .sendRequest().toEntity()
 }
