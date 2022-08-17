4135e4cfe0a62a4c330216a47a64f69bd081d412
Leesin0222 <dragon02229@naver.com>
2022-08-17 21:27:02 +0900

    ⚡ :: RemoteChestDataSource에 스폐셜 상자 오픈시간 조회 interface 함수 작성

--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChestDataSource.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChestDataSource.kt
@@ -1,7 +1,10 @@
 package com.yongjincompany.data.remote.datasource
 
 import com.yongjincompany.domain.entity.chests.FetchFreeChestTimeEntity
+import com.yongjincompany.domain.entity.chests.FetchSpecialChestTimeEntity
 
 interface RemoteChestDataSource {
-    suspend fun fetchFreeChestTime():FetchFreeChestTimeEntity
+    suspend fun fetchFreeChestTime(): FetchFreeChestTimeEntity
+
+    suspend fun fetchSpecialChestTime(): FetchSpecialChestTimeEntity
 }
