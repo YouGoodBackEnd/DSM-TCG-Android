4336af7634ffe9e2b9462c697dfeae9a26f287cd
Leesin0222 <dragon02229@naver.com>
2022-08-17 21:20:27 +0900

    ⚡ :: 스폐셜 상자 시간 조회 api 작성

--- data/src/main/java/com/yongjincompany/data/remote/api/ChestApi.kt
+++ data/src/main/java/com/yongjincompany/data/remote/api/ChestApi.kt
@@ -1,9 +1,13 @@
 package com.yongjincompany.data.remote.api
 
 import com.yongjincompany.data.remote.response.chests.FreeChestOpenResponse
+import com.yongjincompany.data.remote.response.chests.SpecialChestOpenResponse
 import retrofit2.http.GET
 
 interface ChestApi {
     @GET("chests/free")
     suspend fun getFreeChestTime(): FreeChestOpenResponse
+
+    @GET("chests/special")
+    suspend fun getSpecialChestTime(): SpecialChestOpenResponse
 }
