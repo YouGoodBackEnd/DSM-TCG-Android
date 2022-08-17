c4f40b1960ec4d87a4b7969e6bae404b8a1fba17
Leesin0222 <dragon02229@naver.com>
2022-08-17 21:28:21 +0900

    ⚡ :: ChestRepository에 스폐셜 상자 시간조회 interface 작성

--- domain/src/main/java/com/yongjincompany/domain/repository/ChestRepository.kt
+++ domain/src/main/java/com/yongjincompany/domain/repository/ChestRepository.kt
@@ -1,8 +1,11 @@
 package com.yongjincompany.domain.repository
 
 import com.yongjincompany.domain.entity.chests.FetchFreeChestTimeEntity
+import com.yongjincompany.domain.entity.chests.FetchSpecialChestTimeEntity
 import kotlinx.coroutines.flow.Flow
 
 interface ChestRepository {
     suspend fun fetchFreeChestTime(): Flow<FetchFreeChestTimeEntity>
+
+    suspend fun fetchSpecialChestTime(): Flow<FetchSpecialChestTimeEntity>
 }
