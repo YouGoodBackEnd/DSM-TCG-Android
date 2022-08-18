00ab013e185c8504542e7f76234419c8e8c02c9a
Leesin0222 <dragon02229@naver.com>
2022-08-18 21:06:17 +0900

    ⚰️ :: 명세 변경으로 인한 CardRepository & Impl 에서 무료 카드 뽑기 관련 코드 제거

--- data/src/main/java/com/yongjincompany/data/repository/CardRepositoryImpl.kt
+++ data/src/main/java/com/yongjincompany/data/repository/CardRepositoryImpl.kt
@@ -2,11 +2,8 @@ package com.yongjincompany.data.repository
 
 import com.yongjincompany.data.local.datasource.LocalCardDataSource
 import com.yongjincompany.data.remote.datasource.RemoteCardDataSource
-import com.yongjincompany.data.remote.datasource.RemoteCardDataSourceImpl
 import com.yongjincompany.data.util.OfflineCacheUtil
 import com.yongjincompany.domain.entity.cards.FetchMyCardEntity
-import com.yongjincompany.domain.entity.cards.FreeCardEntity
-import com.yongjincompany.domain.entity.users.FetchMyInfoEntity
 import com.yongjincompany.domain.repository.CardRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -15,10 +12,6 @@ class CardRepositoryImpl @Inject constructor(
     private val remoteCardDataSource: RemoteCardDataSource,
     private val localCardDataSource: LocalCardDataSource
 ): CardRepository {
-    override suspend fun drawFreeCard(): Flow<FreeCardEntity> {
-        TODO("Not yet implemented")
-    }
-
     override suspend fun fetchMyCard(): Flow<FetchMyCardEntity> =
         OfflineCacheUtil<FetchMyCardEntity>()
             .remoteData { remoteCardDataSource.fetchMyCard() }
