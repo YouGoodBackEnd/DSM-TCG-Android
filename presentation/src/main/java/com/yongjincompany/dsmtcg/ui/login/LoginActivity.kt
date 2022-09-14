1bcc748758b4e0d877ed0b2cc41bff7f60e28fba
Leesin0222 <dragon02229@naver.com>
2022-09-14 20:21:09 +0900

    ⚡ :: 로그인 구현(ViewModel과 View연결)

--- presentation/src/main/java/com/yongjincompany/dsmtcg/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/ui/login/LoginActivity.kt
@@ -2,34 +2,78 @@ package com.yongjincompany.dsmtcg.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.yongjincompany.dsmtcg.R
 import com.yongjincompany.dsmtcg.base.BaseActivity
 import com.yongjincompany.dsmtcg.databinding.ActivityLoginBinding
+import com.yongjincompany.dsmtcg.extensions.repeatOnStarted
 import com.yongjincompany.dsmtcg.ui.MainActivity
+import com.yongjincompany.dsmtcg.viewmodel.login.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(
     R.layout.activity_login
 ) {
+
+    private val vm: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.btLogin.setOnClickListener {
+            loginToast()
+        }
 
         binding.btLogin.setOnClickListener {
             val accountId: String = binding.etId.text.toString()
             val password: String = binding.etPassword.text.toString()
 
-
+            vm.postLogin(accountId, password)
         }
 
         binding.ibBack.setOnClickListener {
             finish()
         }
-        binding.btLogin.setOnClickListener {
-            intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
         }
     }
 
+    private fun handleEvent(event: LoginViewModel.Event) = when (event) {
+        is LoginViewModel.Event.LoginSuccess-> {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        is LoginViewModel.Event.NoInternet -> showShortToast("인터넷 연결을 확인해주세요.")
+
+        is LoginViewModel.Event.BadRequest -> showShortToast("잘못된 요청입니다. 요청한 값을 확인해주세요.")
+
+        is LoginViewModel.Event.WrongAccount -> showShortToast("아이디나 비밀번호가 틀립니다.")
+
+        else -> showShortToast("알 수 없는 에러가 발생하였습니다.")
+    }
+
+    private fun loginToast() {
+        val id = binding.etId.text
+        val password = binding.etPassword.text
+        if (id.isEmpty()) {
+            Toast.makeText(this, "아이디를 입력해주세요", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (id.isNotEmpty() && password.isEmpty()){
+            Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (id.isNotEmpty() && password.isNotEmpty()){
+
+        }
+    }
 
     override fun initView() {
 
