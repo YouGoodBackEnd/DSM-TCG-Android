814f483bec6f19aa18923e258ca3d0a895d46e04
Leesin0222 <dragon02229@naver.com>
2022-09-17 22:39:29 +0900

    ⚡ :: MainActivity에 AndroidEntryPoint 붙힘

--- presentation/src/main/java/com/yongjincompany/dsmtcg/ui/MainActivity.kt
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/ui/MainActivity.kt
@@ -7,17 +7,20 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.yongjincompany.dsmtcg.base.BaseActivity
 import com.yongjincompany.dsmtcg.R
 import com.yongjincompany.dsmtcg.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(
     R.layout.activity_main
 ) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     private fun initBottomNav() {
         val navController = supportFragmentManager.findFragmentById(R.id.navHostFragmentActivityMain)?.findNavController()
-        val nav = binding.bottomNav as BottomNavigationView
+        val nav = binding.bottomNav
         navController?.let {
             nav.setupWithNavController(navController)
         }
