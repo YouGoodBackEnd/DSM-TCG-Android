573c6dae7b811e98ee329f192b8eae641d901aa8
Leesin0222 <dragon02229@naver.com>
2022-09-17 21:51:47 +0900

    ⚡ :: 홈화면에서 내 정보 불러오기 구현

--- presentation/src/main/java/com/yongjincompany/dsmtcg/ui/home/HomeFragment.kt
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/ui/home/HomeFragment.kt
@@ -5,22 +5,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.yongjincompany.domain.entity.users.FetchMyInfoEntity
 import com.yongjincompany.dsmtcg.R
 import com.yongjincompany.dsmtcg.base.BaseFragment
 import com.yongjincompany.dsmtcg.databinding.FragmentHomeBinding
-import com.yongjincompany.dsmtcg.ui.MyPageActivity
+import com.yongjincompany.dsmtcg.extensions.loadCircleFromUrl
+import com.yongjincompany.dsmtcg.extensions.repeatOnStarted
+import com.yongjincompany.dsmtcg.ui.home.profile.MyPageActivity
+import com.yongjincompany.dsmtcg.viewmodel.home.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
-class  HomeFragment: BaseFragment<FragmentHomeBinding>(
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding>(
     R.layout.fragment_home
 ) {
+    private val vm: HomeViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+        vm.fetchMyInfoValue()
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }
 
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun handleEvent(event: HomeViewModel.Event) = when (event) {
+        is HomeViewModel.Event.FetchMyInfo -> {
+            setProfileValue(event.fetchMyInfoEntity)
+        }
+        is HomeViewModel.Event.ErrorMessage -> {
+            showShortToast(event.message)
+        }
+
     }
 
     override fun initView() {
@@ -28,5 +51,13 @@ class  HomeFragment: BaseFragment<FragmentHomeBinding>(
             val intent = Intent(context, MyPageActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun setProfileValue(profileData: FetchMyInfoEntity) {
+        binding.name.text = profileData.name
+        val tvRank = binding.tvRank
+        if (tvRank.text == "0") tvRank.text = profileData.rank.toString() else tvRank.text =
+            "랭크 순위권 외"
+        profileData.profileImageUrl.let { binding.myImage.loadCircleFromUrl(it) }
     }
 }
