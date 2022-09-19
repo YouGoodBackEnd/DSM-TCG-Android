8701466ea3c3b97955619985d4185ec3a6564ed8
Leesin0222 <dragon02229@naver.com>
2022-09-19 19:39:04 +0900

    ⚡ :: 무료 상자 열기 Response 작성 및 toEntity()화

--- data/src/main/java/com/yongjincompany/data/remote/response/chests/FreeChestOpenResponse.kt
+++ data/src/main/java/com/yongjincompany/data/remote/response/chests/FreeChestOpenResponse.kt
@@ -1,15 +1,35 @@
 package com.yongjincompany.data.remote.response.chests
 
 import com.google.gson.annotations.SerializedName
-import com.yongjincompany.domain.entity.chests.FetchFreeChestTimeEntity
+import com.yongjincompany.domain.entity.chests.FreeChestOpenEntity
 
 data class FreeChestOpenResponse(
-    @SerializedName("chest_open_date_time") val chestOpenDateTime: String,
-    @SerializedName("is_opened") val isOpened: Boolean,
-)
+    @SerializedName("card_list") val cardList: List<Card>,
+    @SerializedName("coin") val coin: Int,
+    @SerializedName("diamond") val diamond: Int,
+) {
+    data class Card(
+        @SerializedName("card_image_url") val cardImageUrl: String,
+        @SerializedName("card_name") val cardName: String,
+        @SerializedName("description") val description: String,
+        @SerializedName("grade") val grade: String,
+        @SerializedName("id") val id: Int,
+    )
+
+}
 
 fun FreeChestOpenResponse.toEntity() =
-    FetchFreeChestTimeEntity(
-        chestOpenDateTime = chestOpenDateTime,
-        isOpened = isOpened
+    FreeChestOpenEntity(
+        cardList = cardList.map { it.toEntity() },
+        coin = coin,
+        diamond = diamond
+    )
+
+fun FreeChestOpenResponse.Card.toEntity() =
+    FreeChestOpenEntity.Card(
+        cardImageUrl = cardImageUrl,
+        cardName = cardName,
+        description = description,
+        grade = grade,
+        id = id
     )
