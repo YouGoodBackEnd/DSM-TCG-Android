d63572783c3c53002c262d77c6c83a1c2a2e678d
Leesin0222 <dragon02229@naver.com>
2022-09-19 20:07:56 +0900

    ⚡ :: RepositoryImpl에 무료 상자 열기 구현체 작성

--- data/src/main/java/com/yongjincompany/data/repository/ChestRepositoryImpl.kt
+++ data/src/main/java/com/yongjincompany/data/repository/ChestRepositoryImpl.kt
@@ -4,13 +4,14 @@ import com.yongjincompany.data.remote.datasource.RemoteChestDataSource
 import com.yongjincompany.data.util.OfflineCacheUtil
 import com.yongjincompany.domain.entity.chests.FetchFreeChestTimeEntity
 import com.yongjincompany.domain.entity.chests.FetchSpecialChestTimeEntity
+import com.yongjincompany.domain.entity.chests.FreeChestOpenEntity
 import com.yongjincompany.domain.repository.ChestRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ChestRepositoryImpl @Inject constructor(
-    private val remoteChestDataSource: RemoteChestDataSource
-): ChestRepository {
+    private val remoteChestDataSource: RemoteChestDataSource,
+) : ChestRepository {
     override suspend fun fetchFreeChestTime(): Flow<FetchFreeChestTimeEntity> =
         OfflineCacheUtil<FetchFreeChestTimeEntity>()
             .remoteData { remoteChestDataSource.fetchFreeChestTime() }
@@ -19,5 +20,10 @@ class ChestRepositoryImpl @Inject constructor(
     override suspend fun fetchSpecialChestTime(): Flow<FetchSpecialChestTimeEntity> =
         OfflineCacheUtil<FetchSpecialChestTimeEntity>()
             .remoteData { remoteChestDataSource.fetchSpecialChestTime() }
+            .createRemoteFlow()
+
+    override suspend fun openFreeChest(): Flow<FreeChestOpenEntity> =
+        OfflineCacheUtil<FreeChestOpenEntity>()
+            .remoteData { remoteChestDataSource.openFreeChest() }
             .createRemoteFlow()
 }
