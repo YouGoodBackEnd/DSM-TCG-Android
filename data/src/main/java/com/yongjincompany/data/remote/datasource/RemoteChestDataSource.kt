63c81240147c27978f55b3be506a0eaee741248b
Leesin0222 <dragon02229@naver.com>
2022-09-19 19:52:57 +0900

    ⚡ :: DataSource에 무료 상자 열기 suspend 함수 작성

--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChestDataSource.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChestDataSource.kt
@@ -1,10 +1,14 @@
 package com.yongjincompany.data.remote.datasource
 
+import com.yongjincompany.data.remote.response.chests.FreeChestOpenResponse
 import com.yongjincompany.domain.entity.chests.FetchFreeChestTimeEntity
 import com.yongjincompany.domain.entity.chests.FetchSpecialChestTimeEntity
+import com.yongjincompany.domain.entity.chests.FreeChestOpenEntity
 
 interface RemoteChestDataSource {
     suspend fun fetchFreeChestTime(): FetchFreeChestTimeEntity
 
     suspend fun fetchSpecialChestTime(): FetchSpecialChestTimeEntity
+
+    suspend fun openFreeChest(): FreeChestOpenEntity
 }
