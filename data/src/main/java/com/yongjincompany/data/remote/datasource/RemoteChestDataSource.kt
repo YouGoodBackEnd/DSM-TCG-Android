426266ad8369a16c2707a7281c4972c84c0847bf
Leesin0222 <dragon02229@naver.com>
2022-09-21 18:50:53 +0900

    ⚡ :: dataSource에 특급 상자 열기 suspend 함수 작성

--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChestDataSource.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChestDataSource.kt
@@ -4,6 +4,7 @@ import com.yongjincompany.data.remote.response.chests.FreeChestOpenResponse
 import com.yongjincompany.domain.entity.chests.FetchFreeChestTimeEntity
 import com.yongjincompany.domain.entity.chests.FetchSpecialChestTimeEntity
 import com.yongjincompany.domain.entity.chests.FreeChestOpenEntity
+import com.yongjincompany.domain.entity.chests.SpecialChestOpenEntity
 
 interface RemoteChestDataSource {
     suspend fun fetchFreeChestTime(): FetchFreeChestTimeEntity
@@ -11,4 +12,6 @@ interface RemoteChestDataSource {
     suspend fun fetchSpecialChestTime(): FetchSpecialChestTimeEntity
 
     suspend fun openFreeChest(): FreeChestOpenEntity
+
+    suspend fun openSpecialChest(): SpecialChestOpenEntity
 }
