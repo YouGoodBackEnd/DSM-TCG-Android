13deac255649e372805dec4f71a199ef331fdbfa
Leesin0222 <dragon02229@naver.com>
2022-09-21 18:55:54 +0900

    ⚡ :: repository에 특급 상자 열기 suspend 함수 작성

--- domain/src/main/java/com/yongjincompany/domain/repository/ChestRepository.kt
+++ domain/src/main/java/com/yongjincompany/domain/repository/ChestRepository.kt
@@ -3,6 +3,7 @@ package com.yongjincompany.domain.repository
 import com.yongjincompany.domain.entity.chests.FetchFreeChestTimeEntity
 import com.yongjincompany.domain.entity.chests.FetchSpecialChestTimeEntity
 import com.yongjincompany.domain.entity.chests.FreeChestOpenEntity
+import com.yongjincompany.domain.entity.chests.SpecialChestOpenEntity
 import kotlinx.coroutines.flow.Flow
 
 interface ChestRepository {
@@ -11,4 +12,6 @@ interface ChestRepository {
     suspend fun fetchSpecialChestTime(): Flow<FetchSpecialChestTimeEntity>
 
     suspend fun openFreeChest(): Flow<FreeChestOpenEntity>
+
+    suspend fun openSpecialChest(): Flow<SpecialChestOpenEntity>
 }
