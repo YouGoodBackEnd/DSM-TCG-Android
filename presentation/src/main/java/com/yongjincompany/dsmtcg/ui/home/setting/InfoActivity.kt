1fc08aeee62274585df0422e9079c35bae5b7a17
Leesin0222 <dragon02229@naver.com>
2022-09-25 20:51:20 +0900

    ⚡ :: infoActivity

---
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/ui/home/setting/InfoActivity.kt
@@ -0,0 +1,18 @@
+package com.yongjincompany.dsmtcg.ui.home.setting
+
+import android.os.Bundle
+import com.yongjincompany.dsmtcg.R
+import com.yongjincompany.dsmtcg.base.BaseActivity
+import com.yongjincompany.dsmtcg.databinding.ActivityInfoBinding
+
+class InfoActivity: BaseActivity<ActivityInfoBinding>(
+    R.layout.activity_info
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun initView() {
+
+    }
+}
