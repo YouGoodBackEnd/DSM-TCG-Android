c641405f636c4039abca4d2c9a8d7c02f472b9aa
Leesin0222 <dragon02229@naver.com>
2022-09-25 20:40:45 +0900

    ⚡ :: 실버 상자 뷰모델 작성

---
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/viewmodel/chest/SilverChestViewModel.kt
@@ -0,0 +1,66 @@
+package com.yongjincompany.dsmtcg.viewmodel.chest
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yongjincompany.domain.entity.chests.SilverChestOpenEntity
+import com.yongjincompany.domain.exception.ConflictException
+import com.yongjincompany.domain.exception.UnauthorizedException
+import com.yongjincompany.domain.usecase.chest.OpenSilverChestUseCase
+import com.yongjincompany.dsmtcg.util.MutableEventFlow
+import com.yongjincompany.dsmtcg.util.asEventFlow
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SilverChestViewModel @Inject constructor(
+    private val openSilverChestUseCase: OpenSilverChestUseCase,
+) : ViewModel() {
+
+    private val _eventFlow = MutableEventFlow<Event>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    fun openSilverChestValue() {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                openSilverChestUseCase.execute(Unit).collect {
+                    event(Event.OpenSilverChest(it.toData()))
+                }
+            }.onFailure {
+                when (it) {
+                    is ConflictException -> event(Event.ErrorMessage("상자 오픈 시간이 아닙니다"))
+                    is UnauthorizedException -> event(Event.ErrorMessage("토큰이 만료되었습니다. 다시 로그인해주세요."))
+                    else -> event(Event.ErrorMessage("알 수 없는 에러가 발생했습니다."))
+                }
+            }
+        }
+    }
+
+    private fun SilverChestOpenEntity.toData() =
+        SilverChestOpenEntity(
+            cardList = cardList.map { it.toData() },
+            coin = coin,
+            diamond = diamond
+        )
+
+    private fun SilverChestOpenEntity.Card.toData() =
+        SilverChestOpenEntity.Card(
+            cardImageUrl = cardImageUrl,
+            cardName = cardName,
+            description = description,
+            grade = grade,
+            id = id
+        )
+
+
+    private fun event(event: Event) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
+
+    sealed class Event {
+        data class OpenSilverChest(val silverChestOpenEntity: SilverChestOpenEntity) : Event()
+        data class ErrorMessage(val message: String) : Event()
+    }
+}
