2f7b4a4b62554e6a1fc5db5dd2d49e6ca13ac8cf
Leesin0222 <dragon02229@naver.com>
2022-09-25 20:52:02 +0900

    ⚡ :: 카드 상세보기 Activity 생성

---
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/ui/mycard/CardDetailActivity.kt
@@ -0,0 +1,19 @@
+package com.yongjincompany.dsmtcg.ui.mycard
+
+import android.os.Bundle
+import com.yongjincompany.dsmtcg.R
+import com.yongjincompany.dsmtcg.base.BaseActivity
+import com.yongjincompany.dsmtcg.databinding.ActivityCardDetailBinding
+
+class CardDetailActivity : BaseActivity<ActivityCardDetailBinding>(
+    R.layout.activity_card_detail
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun initView() {
+
+    }
+
+}
