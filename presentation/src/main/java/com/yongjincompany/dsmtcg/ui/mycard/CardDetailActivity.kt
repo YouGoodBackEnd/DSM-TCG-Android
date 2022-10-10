c6d919ad4c5359eab01966b709e2b2c6611beeae
hou <dragon02229@naver.com>
2022-10-10 18:35:48 +0900

    ⚡ :: 카드 상세보기 구현

--- presentation/src/main/java/com/yongjincompany/dsmtcg/ui/mycard/CardDetailActivity.kt
+++ presentation/src/main/java/com/yongjincompany/dsmtcg/ui/mycard/CardDetailActivity.kt
@@ -4,16 +4,35 @@ import android.os.Bundle
 import com.yongjincompany.dsmtcg.R
 import com.yongjincompany.dsmtcg.base.BaseActivity
 import com.yongjincompany.dsmtcg.databinding.ActivityCardDetailBinding
+import com.yongjincompany.dsmtcg.extensions.loadFromUrl
 
 class CardDetailActivity : BaseActivity<ActivityCardDetailBinding>(
     R.layout.activity_card_detail
 ) {
+    private var description: String = ""
+    private var name: String = ""
+    private var grade: String = ""
+    private var cardImage: String = ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        description = intent.getStringExtra("DESCRIPTION").toString()
+        name = intent.getStringExtra("NAME").toString()
+        grade = intent.getStringExtra("GRADE").toString()
+        cardImage = intent.getStringExtra("CARD_IMAGE").toString()
     }
-    override fun initView() {
 
+    override fun initView() {
+        binding.ivCard.loadFromUrl(cardImage)
+        binding.cardExplain.text = description
+        binding.cardName2.text = name
+        binding.cardGrade2.text = grade
+
+        binding.ivExit.setOnClickListener {
+            finish()
+        }
     }
 
 }
